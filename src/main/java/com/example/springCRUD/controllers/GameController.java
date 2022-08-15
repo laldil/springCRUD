e37bd571fc4f529c035b82b214bb75c7e528abcc
aldik <aldi001122@gmail.com>
2022-08-16 01:14:37 +0600

    Some changes

--- src/main/java/com/example/springCRUD/controllers/GameController.java
+++ src/main/java/com/example/springCRUD/controllers/GameController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 
 @Controller
@@ -25,6 +29,17 @@ public class GameController {
         model.addAttribute("games", games);
         model.addAttribute("title", "Games");
         return "game";
+    }
+
+    @GetMapping("/game/{id}")
+    public String showGameInfoPage(@PathVariable(value = "id") long id, Model model){
+        if(!gameRepository.existsById(id)) return "redirect:/game";
+
+        Optional<GameModel> game = gameRepository.findById(id);
+        ArrayList<GameModel> result =new ArrayList<>();
+        game.ifPresent(result :: add);
+        model.addAttribute("game", result);
+        return "gameBlog";
     }
 
     @GetMapping("/game/create")
